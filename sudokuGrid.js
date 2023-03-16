2d2aad7dde6edd443fa1b96bb13c5e7899aaa01a
Oscaperion <brandondancer@protonmail.com>
2023-03-17 00:46:26 +0200

    Added subgrid related ID for cells, tried some alternate ways to get rows, cols and subgrids and added an additional cell check on fillApplicableSpaces. This was able to figure out the harder sudoku I had added after the first one.

--- sudokuGrid.js
+++ sudokuGrid.js
@@ -17,12 +17,18 @@ function col(colNumber) { return String.fromCharCode(startLetterPos - 1 + colNum
       
       for (let j = 1; j <= 9; j++) {
          let tmp2 = tmp1 + j;
+         
+         // 1 = Top-left, 2 = Top-middle, 3 = Top-right
+         // 4 = Middle-left, 5 = Middle, 6 = Middle-right
+         // 7 = Bottom-left, 8 = Bottom-middle, 9 = Bottom-right
+         let subgrid = Math.floor((j - 1) / 3) * 3 + Math.floor((i - 1) / 3) + 1;
          sudokuGrid[tmp2] = {};
          
          // Each cell will have information what the assigned number is (value) and - if still not assigned - what other numbers it could still be
          sudokuGrid[tmp2]["id"] = tmp2;
          sudokuGrid[tmp2]["col"] = i;
          sudokuGrid[tmp2]["row"] = j;
+         sudokuGrid[tmp2]["subgrid"] = subgrid;
          sudokuGrid[tmp2]["value"] = 0;
          sudokuGrid[tmp2]["assigned"] = false;
          
@@ -34,7 +40,7 @@ function col(colNumber) { return String.fromCharCode(startLetterPos - 1 + colNum
    //sudokuGrid["E4"].value = 5;
 
 }
-
+    /*
 function getRow(rowNumber) {
    if (rowNumber > 9 || rowNumber < 1) throw new Error('Value not compatible! Needs to be within 1-9 range');
 
@@ -43,24 +49,37 @@ function getRow(rowNumber) {
    for (let i = 1; i <= 9; i++) {
       returnArray.push(col(i) + rowNumber);
    }
-   
+
    return returnArray;
+}  */
+
+function getRow(rowNumber) {
+   let filteredData = Object.values(sudokuGrid).filter(item => item.row === rowNumber);
+   //console.log(filteredData.map(item => item.id));
+   return filteredData.map(item => item.id);
 }
 
+/*
 function getCol(colNumber) {
    if (colNumber > 9 || colNumber < 1) throw new Error('Value not compatible! Needs to be within 1-9 range');
 
    let returnArray = [];
 
    let colTmp = col(colNumber);
-   
+
    for (let i = 1; i <= 9; i++) {
       returnArray.push(colTmp + i);
    }
-   
-   return returnArray;
-}
 
+   return returnArray;
+} */
+
+function getCol(colNumber) {
+   let filteredData = Object.values(sudokuGrid).filter(item => item.col === colNumber);
+   //console.log(filteredData.map(item => item.id));
+   return filteredData.map(item => item.id);
+}
+     /*
 // 1 = Top-left, 2 = Top-middle, 3 = Top-right
 // 4 = Middle-left, 5 = Middle, 6 = Middle-right
 // 7 = Bottom-left, 8 = Bottom-middle, 9 = Bottom-right
@@ -84,14 +103,21 @@ function getSubGrid(gridId) {
    }
 
    return returnArray;
+}  */
+
+function getSubGrid(gridId) {
+   let filteredData = Object.values(sudokuGrid).filter(item => item.subgrid === gridId);
+   //console.log(filteredData.map(item => item.id));
+   return filteredData.map(item => item.id);
 }
+
                                /*
 console.log(sudokuGrid);
 console.log(sudokuGrid.E4);
 console.log(getRow(4)[4]);
 console.log(getCol(5)[3]);
 console.log(getSubGrid(5));  */
-                    /*
+                           /*
 printGrid();
 setValue("A1",5);
 setValue("B1",3);
@@ -130,7 +156,9 @@ setValue("I8",5);
 
 setValue("E9",8);
 setValue("H9",7);
-setValue("I9",9);
+setValue("I9",9);         */
+
+/*
 console.log(sudokuGrid);
 console.log(getSubGrid(9));
 console.log(isApplicableValue("G8",6));
@@ -176,7 +204,10 @@ setValue("I4",9);
 setValue("I8",1);
 
 printGrid();
-fillApplicableSpaces();
+fillApplicableSpaces();          /*
+console.log(sudokuGrid["F1"]);
+console.log(sudokuGrid["F8"]);
+console.log(sudokuGrid["F9"]); */
 printGrid();
 
 function setValue(cellId,value) {
@@ -186,27 +217,23 @@ function setValue(cellId,value) {
    sudokuGrid[cellId].value = value;
    sudokuGrid[cellId].assigned = true;
    //wipeCell(cellId,value);
-   notifyCells(sudokuGrid[cellId].col, sudokuGrid[cellId].row, value);
+   notifyCells(cellId, value);
 }
 
-function wipeCell(cellId,value) {
-   sudokuGrid[cellId].assigned = true;
-   for (let i = 1; i <= 9; i++) {
-      if (value !== i) sudokuGrid[cellId][indicator + i] = false;
-   }
-}
+function notifyCells(cellId, value) {
+   let tmpId = cellId;
 
-function notifyCells(cellCol, cellRow, value) {
-   let tmpId = col(cellCol) + cellRow;
+   let tmpCol = getCol(sudokuGrid[cellId].col);
+   let tmpRow = getRow(sudokuGrid[cellId].row);
+   let tmpSgrid = getSubGrid(sudokuGrid[cellId].subgrid);
 
-   let tmpCol = getCol(cellCol);
-   let tmpRow = getRow(cellRow);
-
-   for (let i = 1; i <= 9; i++) {
-      sudokuGrid[col(i) + cellRow][indicator + value] = false;
-      sudokuGrid[col(cellCol) + i][indicator + value] = false;
+   for (let i = 0; i < 9; i++) {
+      sudokuGrid[tmpCol[i]][indicator + value] = false;
+      sudokuGrid[tmpRow[i]][indicator + value] = false;
+      sudokuGrid[tmpSgrid[i]][indicator + value] = false;
    }
 
+   /*
    for (let i = 1; i <= 9; i++) {
          //console.log("sas");
       if (getSubGrid(i).includes(tmpId)) {
@@ -219,7 +246,7 @@ function notifyCells(cellCol, cellRow, value) {
          //console.log("sus");
          break;
       }
-   }
+   }    */
 }
 
 function isApplicableValue(cellId,checkValue) {
@@ -255,9 +282,35 @@ function fillApplicableSpaces() {
             if (isApplicableValue(tmpSGrid[j], k)) { applicableCellsSGrid++; applicableCellG = tmpSGrid[j]; }
          }
 
-         if (applicableCellsRow === 1) { setValue(applicableCellR,k); changed = true; }
-         if (applicableCellsCol === 1 && applicableCellC !== applicableCellR) { setValue(applicableCellC,k); changed = true; }
-         if (applicableCellsSGrid === 1 && applicableCellG !== applicableCellR && applicableCellG !== applicableCellC) { setValue(applicableCellG,k); changed = true; }
+         if (applicableCellsRow === 1) {
+            setValue(applicableCellR,k); changed = true; console.log("Setting " + applicableCellR + " as " + k);
+         } if (applicableCellsCol === 1 && applicableCellC !== applicableCellR) {
+            setValue(applicableCellC,k); changed = true; console.log("Setting " + applicableCellC + " as " + k);
+         } if (applicableCellsSGrid === 1 && applicableCellG !== applicableCellR && applicableCellG !== applicableCellC) {
+            setValue(applicableCellG,k); changed = true; console.log("Setting " + applicableCellG + " as " + k);
+         }
+      }
+                
+      for (let y = 1; y <= 9; y++) {
+         for (let x = 1; x <= 9; x++) {
+            let tmpo = 0;
+            let tmpId = "";
+            let tmpValue = 0;
+
+            for (let z = 1; z <= 9; z++) {
+               if (sudokuGrid[col(y) + x][indicator + z] === true) {
+                  tmpo++;
+                  tmpId = col(y) + x;
+                  tmpValue = z;
+               }
+            }
+
+            if (tmpo === 1 && sudokuGrid[tmpId].assigned === false) {
+               setValue(tmpId,tmpValue);
+               changed = true;
+               console.log("Setting " + tmpId + " as " + tmpValue);
+            }
+         }
       }
    }
   }
